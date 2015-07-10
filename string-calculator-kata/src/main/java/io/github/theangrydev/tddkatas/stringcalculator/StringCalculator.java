9e127eb999d1b31676003e4a784368b8ea36df48
Liam Williams <liam.williams@zoho.com>
2015-07-10 21:43:39 +0100

    Refactoring

--- string-calculator-kata/src/main/java/io/github/theangrydev/tddkatas/stringcalculator/StringCalculator.java
+++ string-calculator-kata/src/main/java/io/github/theangrydev/tddkatas/stringcalculator/StringCalculator.java
@@ -1,13 +1,21 @@
 package io.github.theangrydev.tddkatas.stringcalculator;
 
-import static java.util.Arrays.stream;
+import com.google.common.base.Splitter;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class StringCalculator {
 
 	public int add(String numbers) {
-		if (numbers.isEmpty()) {
-			return 0;
-		}
-		return stream(numbers.split(",")).mapToInt(Integer::parseInt).sum();
+		return stream(eachNumber(numbers)).mapToInt(Integer::parseInt).sum();
+	}
+
+	private Iterable<String> eachNumber(String numbers) {
+		return Splitter.on(',').omitEmptyStrings().split(numbers);
+	}
+
+	private static <T> Stream<T> stream(Iterable<T> iterable) {
+		return StreamSupport.stream(iterable.spliterator(), false);
 	}
 }
